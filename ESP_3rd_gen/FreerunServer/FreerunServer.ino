c213fb3982e6fb942e04b7aec1f08fe15d5f791d
freerun <freerun@MacBook-Pro-5.fritz.box>
2020-05-30 21:32:49 +0200

    Server updated.

--- ESP_3rd_gen/FreerunServer/FreerunServer.ino
+++ ESP_3rd_gen/FreerunServer/FreerunServer.ino
@@ -1,45 +1,51 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h> 
+#include <ESP8266WebServer.h>
 
 
-// Soft Access Point: 
+// Soft Access Point:
 // https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-examples.html
 //
-// WiFiAccessPoint: 
+// WiFiAccessPoint:
 // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/WiFiAccessPoint/WiFiAccessPoint.ino
 
 
 
 const char *ssid = "FreerunButtons";
 // passwd must have length >= 8 : https://github.com/esp8266/Arduino/issues/1141
-const char *password = "freerunforrestfreerun";   
+const char *password = "freerunforrestfreerun";
 
-ESP8266WebServer server(80); 
+ESP8266WebServer server(80);
+
+int lastContactMsec = 0;
 
 
 void handleRoot() {
-  
+
   server.send(200, "text/html","<h1>You are connected</h1>");
   Serial.print("X");
 }
 
+void prn(String name, String value) {
+  Serial.print(" "+name+": ");
+  Serial.print(value);
+}
+
 void handleBtn() {
+  long serverMsec = millis();
   String clientMsec = server.arg("msec");
   String clientId  = server.arg("sender");
   String previousResponseTime = server.arg("prevdur");
   server.send(200, "text/plain", "a b c.");
-  Serial.print("Client_msec[ ");
-  Serial.print(clientMsec);
-  Serial.print(" ] - sender[ ");
-  Serial.print(clientId);
-  Serial.print(" ] - previous request duration [");
-  Serial.print(previousResponseTime);
-  Serial.print(" ] - diff client/server time [");
-  Serial.print( millis()  );
-  Serial.print( " - " );
-  Serial.print( millis() - atol(clientMsec.c_str()) );
-  Serial.println(" ] ");
+  Serial.print("--> ");
+  prn( "Sender", clientId);
+  prn("previousRequestDuration", previousResponseTime);
+  prn("client", clientMsec );
+  prn("server", String(serverMsec) );
+  long offset = serverMsec - atol(clientMsec.c_str());
+  prn("offset", String(offset) );
+  Serial.println();
+  lastContactMsec = serverMsec;
 }
 
 
@@ -49,7 +55,7 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println("Setting soft-AP ... ");
- 
+
   boolean result = WiFi.softAP(ssid, password);
   if(result)
   {
@@ -69,19 +75,29 @@ void setup()
 }
 
 
+
 int count = 0;
+int numsoftAPConnectedStations = -1;
+
+
+bool isLongerThanXSecondsApart(long moment1, long moment2, long seconds) {
+  long duration = moment1 < moment2  ?  moment2 - moment1  :  moment1 - moment2 ;
+  return duration > 1000 * seconds ;
+}
 
 void loop()
 {
   server.handleClient();
   ++count;
-  if (count % 1000000 == 0) 
-  { 
-    Serial.print(millis()); 
-    Serial.print(" - ");
-    Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-    Serial.println();
+  if (count % 1000000 == 0)
+  {
+    // WiFi.softAPgetStationNum() changes ? Then print!
+    int num = WiFi.softAPgetStationNum();
+    if  (num != numsoftAPConnectedStations || isLongerThanXSecondsApart(lastContactMsec, millis(), 60) )
+    {
+      Serial.print(millis());
+      Serial.print(" - ");
+      Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+    }
   }
-  //
-  //delay(3000);
 }
