f3f747b3bce704d630a7a1fa0be9f5a56260c3b7
freerun <freerun@MacBook-Pro-5.fritz.box>
2020-05-30 22:45:41 +0200

    Server update.

--- ESP_3rd_gen/FreerunServer/FreerunServer.ino
+++ ESP_3rd_gen/FreerunServer/FreerunServer.ino
@@ -1,34 +1,34 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+#include <ESP8266WebServer.h> 
 
 
-// Soft Access Point:
+// Soft Access Point: 
 // https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-examples.html
 //
-// WiFiAccessPoint:
+// WiFiAccessPoint: 
 // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/WiFiAccessPoint/WiFiAccessPoint.ino
 
 
 
 const char *ssid = "FreerunButtons";
 // passwd must have length >= 8 : https://github.com/esp8266/Arduino/issues/1141
-const char *password = "freerunforrestfreerun";
+const char *password = "freerunforrestfreerun";   
+ 
+ESP8266WebServer server(80); 
 
-ESP8266WebServer server(80);
-
-int lastContactMsec = 0;
+int lastContactMsec = 0; 
 
 
 void handleRoot() {
-
+  
   server.send(200, "text/html","<h1>You are connected</h1>");
   Serial.print("X");
 }
 
 void prn(String name, String value) {
   Serial.print(" "+name+": ");
-  Serial.print(value);
+  Serial.print(value); 
 }
 
 void handleBtn() {
@@ -55,7 +55,7 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println("Setting soft-AP ... ");
-
+ 
   boolean result = WiFi.softAP(ssid, password);
   if(result)
   {
@@ -82,22 +82,24 @@ int numsoftAPConnectedStations = -1;
 
 bool isLongerThanXSecondsApart(long moment1, long moment2, long seconds) {
   long duration = moment1 < moment2  ?  moment2 - moment1  :  moment1 - moment2 ;
-  return duration > 1000 * seconds ;
+  return duration > 1000 * seconds ;  
 }
 
 void loop()
 {
   server.handleClient();
   ++count;
-  if (count % 1000000 == 0)
-  {
-    // WiFi.softAPgetStationNum() changes ? Then print!
+  if (count % 1000000 == 0) 
+  { 
+    // WiFi.softAPgetStationNum() changes ? Then print! 
     int num = WiFi.softAPgetStationNum();
-    if  (num != numsoftAPConnectedStations || isLongerThanXSecondsApart(lastContactMsec, millis(), 60) )
+    if  (num != numsoftAPConnectedStations || isLongerThanXSecondsApart(lastContactMsec, millis(), 60) ) 
     {
-      Serial.print(millis());
-      Serial.print(" - ");
-      Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+      numsoftAPConnectedStations = num;
+      lastContactMsec = millis();
+      Serial.println();
+      Serial.print(" --- IDLE at: "+ millis());
+      Serial.printf(" stationsConnected: %d\n", num);
     }
   }
 }
