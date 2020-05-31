93f1b6e5af8f1581bfdd9aebc459d100ff30f719
freerun <freerun@MacBook-Pro-5.fritz.box>
2020-05-31 21:16:16 +0200

    simple server output ready.

--- ESP_3rd_gen/FreerunServer/FreerunServer.ino
+++ ESP_3rd_gen/FreerunServer/FreerunServer.ino
@@ -20,32 +20,119 @@ ESP8266WebServer server(80);
 int lastContactMsec = 0; 
 
 
+const int numberOfButtons = 3;
+const int bufferSize = numberOfButtons * 10;
+long pressedStore [bufferSize];  // circular array, 'crsr' pointing to next free cel. 
+int crsr = 0; 
+
+
+void clearStore() 
+{
+  for(int i=0 ; i < bufferSize ; ++i )
+  {
+    pressedStore[i] = 0;
+  }
+}
+
+
+void addPress(long timeOfPress) 
+{
+  pressedStore[crsr] = timeOfPress ;         // not yet very precise... 
+  ++crsr;
+}
+
+
+//  pressedStore[i] = 0;  // fake time to begin with. 
+//  pressedStore[i] = 0;  // fake time to begin with. 
+//  pressedStore[i] = 0;  // fake time to begin with. 
+
+
+void showStore()
+{
+  Serial.println();
+  for(int i=0 ; i < bufferSize ; ++i )
+  {
+    if (i == crsr) { Serial.print(" [[[ "); } 
+    Serial.print(pressedStore[i]);
+    if (i == crsr) { Serial.print(" ]]] "); } 
+    Serial.print(".");
+  }
+  Serial.println();
+}
+
+
+
 void handleRoot() {
   
   server.send(200, "text/html","<h1>You are connected</h1>");
   Serial.print("X");
 }
 
+
 void prn(String name, String value) {
   Serial.print(" "+name+": ");
   Serial.print(value); 
 }
 
+
+void showTimes()
+{
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println("---------------------------------------------");
+  Serial.println();
+  int t1Index = ((crsr - 1) / 3) * 3; 
+  long t1 = pressedStore[ t1Index ];
+  long t2 = pressedStore[ t1Index + 1 ];
+  long t3 = pressedStore[ t1Index + 2 ];
+  long halfTime = t2 - t1;
+  long endTime  = t3 - t1;
+  prn("crsr", String(crsr));
+  prn("t1Index", String(t1Index));
+  switch( crsr % 3 ) {
+    case 1:   
+              Serial.println("-- Started --");
+              break; 
+    case 2:   
+              prn("Halftime   ", String( halfTime ));
+              Serial.println();
+              break; 
+    case 0: 
+              prn("Halftime   ", String( halfTime ));
+              Serial.println();
+              prn("Endtime    ", String( endTime ));
+              Serial.println();
+              break;
+  }
+  Serial.println("---------------------------------------------");
+  Serial.println();
+  Serial.println();
+  Serial.println();
+}
+
+
 void handleBtn() {
   long serverMsec = millis();
-  String clientMsec = server.arg("msec");
+  String clientMsecAsString = server.arg("msec");
+  long   clientMsec = atol(clientMsecAsString.c_str());
   String clientId  = server.arg("sender");
   String previousResponseTime = server.arg("prevdur");
+  String buttonIP = server.arg("ip");
   server.send(200, "text/plain", "a b c.");
-  Serial.print("--> ");
-  prn( "Sender", clientId);
+  Serial.print("--> ButtonPress ");
+  prn( "button", clientId);
   prn("previousRequestDuration", previousResponseTime);
-  prn("client", clientMsec );
-  prn("server", String(serverMsec) );
-  long offset = serverMsec - atol(clientMsec.c_str());
+  prn("clientTime", clientMsecAsString );
+  prn("serverTime", String(serverMsec) );
+  long offset = serverMsec - clientMsec;
   prn("offset", String(offset) );
+  prn("buttonIP", buttonIP );
   Serial.println();
   lastContactMsec = serverMsec;
+  long serverTimeOfPress = clientMsec + offset; 
+  addPress(serverTimeOfPress);
+  showTimes();
 }
 
 
@@ -70,8 +157,12 @@ void setup()
   }
   else
   {
-    Serial.println("- Setting up Soft Access Point Failed!");
+    Serial.println("- Setting up Soft Access Point Failed! ----- PLEASE RESTART -----");
   }
+  clearStore();
+  addPress(1);  // fake data to test with... 
+  addPress(3);  // fake data to test with... 
+  addPress(8);  // fake data to test with... 
 }
 
 
@@ -84,6 +175,7 @@ bool isLongerThanXSecondsApart(long moment1, long moment2, long seconds) {
   long duration = moment1 < moment2  ?  moment2 - moment1  :  moment1 - moment2 ;
   return duration > 1000 * seconds ;  
 }
+
 
 void loop()
 {
@@ -100,6 +192,8 @@ void loop()
       Serial.println();
       Serial.print(" --- IDLE at: "+ millis());
       Serial.printf(" stationsConnected: %d\n", num);
+      showStore();
+      showTimes();
     }
   }
 }
